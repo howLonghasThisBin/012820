a612328c5377203e13ba71dd4d886f2c9d4b0900
howLonghasThisBin <mlonghas_180000001821@uic.edu.ph>
2020-01-28 11:03:42 +0800

    added HELLONODE activity via git

---
+++ HELLONODE/node_script1.js
@@ -0,0 +1,11 @@
+// console.log("Hello Node");
+
+class A {
+    constructor(){ console.log("Constructor");}
+    getMessage(){
+        console.log("Hello Node");
+    }
+}
+
+a = new A();
+a.getMessage();
